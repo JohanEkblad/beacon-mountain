c5f75d1042d771b44fd75009b738e0a5ad40cba6
Samuel Moritz <samuel.moritz@gmail.com>
2017-10-13 15:43:27 +0200

    Basic server listening on port 4711

--- android/BeaconMountain/app/src/main/java/se/omegapoint/beaconmountain/MainActivity.java
+++ android/BeaconMountain/app/src/main/java/se/omegapoint/beaconmountain/MainActivity.java
@@ -2,6 +2,14 @@ package se.omegapoint.beaconmountain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.net.ServerSocketFactory;
+import javax.net.SocketFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Log.v("tag","hej");
+                    ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(4711);
+                    Log.v("server",serverSocket.toString());
+                    Socket client;
+                    while((client = serverSocket.accept()) != null) {
+                        Log.v("client", client.toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
