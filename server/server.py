47149b69c91a16e94f9cbfd38d1485fafb5fe8b7
Johan Ekblad <johan.ekblad@umu.se>
2017-10-15 22:59:05 +0200

    Made the server.py compatible with python3

--- server/server.py
+++ server/server.py
@@ -15,10 +15,10 @@ def sendReply(connection, clientString):
   "Send a reply from the client request"
   parts = clientString.split(':')
   if len(parts) != 5:
-    print >> sys.stdout, 'YAPP error: Client message must consists of 5 fields'
+    print('YAPP error: Client message must consists of 5 fields')
     return
   if parts[0] != "HELO":
-    print >> sys.stdout, 'YAPP error: No HELO'
+    print('YAPP error: No HELO')
     return
   nickname=parts[1]
   latitude=parts[2]
@@ -39,21 +39,21 @@ def sendReply(connection, clientString):
       longitude=data[2]
       reply=reply+':'+nickname+':'+str(latitude)+':'+str(longitude)
   else:
-    print >> sys.stdout, 'YAPP error: answer must be "Y" or "N"'
+    print('YAPP error: answer must be "Y" or "N"')
 
   reply=reply+'\0'
   connection.sendall(reply)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('', 4711)
-print >>sys.stdout, 'Starting up on %s port %s' % server_address
+print('Starting up on %s port %s' % server_address)
 sock.bind(server_address)
 sock.listen(5) # Queue up to 5 requests
 while True:
-  print >>sys.stdout, 'Waiting for a client connection'
+  print('Waiting for a client connection')
   connection, client_address = sock.accept()
   try:
-    print >>sys.stdout, 'Connection from', client_address
+    print('Connection from %s' % (client_address))
     clientString=''
     while True:
       data = connection.recv(1)
